f95f99005d94ddfc4449b59de0b67377ba623744
Beziat <@bzk3886a>
2023-03-24 08:32:10 +0100

    creation page inscription

--- app/src/main/java/com/example/trashreport/Inscription.java
+++ app/src/main/java/com/example/trashreport/Inscription.java
@@ -1,4 +1,44 @@
 package com.example.trashreport;
 
-public class Inscription {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Inscription extends AppCompatActivity {
+    Button inscription;
+    Button retour;
+    EditText EMail;
+    EditText MDP;
+    EditText MDPVrai;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.inscription);
+        inscription  = findViewById(R.id.Inscription_Retour);
+        retour = findViewById(R.id.Inscription_Valider);
+        EMail = findViewById(R.id.Inscription_EmailAddress);
+        MDP = findViewById(R.id.Inscription_Password1);
+        MDPVrai = findViewById(R.id.Inscription_Password2);
+        inscription.setClickable(false);
+
+        retour.setOnClickListener(v -> {
+            Intent connexion = new Intent(Inscription.this,MainActivity.class);
+            startActivity(connexion);
+        });
+
+        String eMailText = String.valueOf(EMail.getText());
+        String mDPText = String.valueOf(MDP.getText());
+        String mDPVraiText = String.valueOf(MDPVrai.getText());
+
+        if (mDPText.equals(mDPVraiText)){
+            inscription.setClickable(!mDPText.isEmpty());
+        }else {
+            inscription.setClickable(false);
+        }
+    }
 }
