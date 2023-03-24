5653508d449c8ebdfdad5caf7aad3f0ee63110cd
Beziat <@bzk3886a>
2023-03-24 09:57:14 +0100

    page de dignalement crée

--- app/src/main/java/com/example/trashreport/SignalementDesDéchets.java
+++ app/src/main/java/com/example/trashreport/SignalementDesDéchets.java
@@ -4,28 +4,35 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.example.trashreport.Ressources.SQLClient;
-
-public class Inscription extends AppCompatActivity {
-    Button inscription;
-    Button retour;
-    EditText EMail;
-    EditText MDP;
-    EditText MDPVrai;
+public class SignalementDesDéchets extends AppCompatActivity {
+    RadioButton bricolage;
+    RadioButton verre_plastique;
+    RadioGroup liste;
+    RadioButton epave;
+    RadioButton megot;
+    RadioButton papier;
+    RadioButton gravat;
+    RadioButton autre;
+    EditText detail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.inscription);
-        inscription  = findViewById(R.id.Inscription_Retour);
-        retour = findViewById(R.id.Inscription_Valider);
-        EMail = findViewById(R.id.Inscription_EmailAddress);
-        MDP = findViewById(R.id.Inscription_Password1);
-        MDPVrai = findViewById(R.id.Inscription_Password2);
-        inscription.setClickable(false);
+        setContentView(R.layout.signalementdesdechets);
+        liste = findViewById(R.id.signalementdesdechets_liste);
+        bricolage  = findViewById(R.id.signalementdesdechets_bricolage);
+        verre_plastique = findViewById(R.id.signalementdesdechets_verre_plastique);
+        epave = findViewById(R.id.signalementdesdechets_epave);
+        megot = findViewById(R.id.signalementdesdechets_megot);
+        papier = findViewById(R.id.signalementdesdechets_papier);
+        gravat = findViewById(R.id.signalementdesdechets_gravat);
+        autre = findViewById(R.id.signalementdesdechets_autre);
+        detail = findViewById(R.id.signalementdesdechets_text_autre);
 
         retour.setOnClickListener(v -> {
             Intent connexion = new Intent(Inscription.this,MainActivity.class);
