02e3969c052c56f2913462ea8bbe2a123b9cf167
Beziat <@bzk3886a>
2023-03-24 08:37:29 +0100

    page inscription sans les fonctionnalités liée a la BD terminé

--- app/src/main/java/com/example/trashreport/Inscription.java
+++ app/src/main/java/com/example/trashreport/Inscription.java
@@ -2,11 +2,12 @@ package com.example.trashreport;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.trashreport.Ressources.SQLClient;
 
 public class Inscription extends AppCompatActivity {
     Button inscription;
@@ -40,5 +41,13 @@ public class Inscription extends AppCompatActivity {
         }else {
             inscription.setClickable(false);
         }
+
+        inscription.setOnClickListener(v -> {
+            if (){
+                Intent connexion = new Intent(Inscription.this,MainActivity.class);
+                startActivity(connexion);
+            }
+
+        });
     }
 }
